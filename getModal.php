715e3ac7e41c139c3d8a3067f7fb7e67c466f106
Fabian <fabian.horta@ilikewebsites.com>
2017-07-14 12:44:58 -0500

    fixed search 10 items show

--- getModal.php
+++ getModal.php
@@ -14,38 +14,41 @@ $json = file_get_contents($url);
 $obj = json_decode($json);
 
 if(!empty($obj->data->results[0]->name)){
-?>
 
+for($i = 0;$i<10;$i++){
+
+?>
 <div class="contentHero">
 			<div class="col-md-7">
-				<img class="img-circle" src="<?php echo $obj->data->results[0]->thumbnail->path.".".$obj->data->results[0]->thumbnail->extension?>" width="200" height="200" />
+				<img class="img-circle" src="<?php echo $obj->data->results[$i]->thumbnail->path.".".$obj->data->results[$i]->thumbnail->extension?>" width="200" height="200" />
 			</div>
 			<div class="col-md-5">
-				<span class="titleHero"><?php echo  $obj->data->results[0]->name; ?></span>
+				<span class="titleHero"><?php echo  $obj->data->results[$i]->name; ?></span>
 			</div>
-			<span class="col-md-5"><?php echo  substr($obj->data->results[0]->description,100); ?>...</span>
+			<span class="col-md-5"><?php echo  substr($obj->data->results[$i]->description,100); ?>...</span>
 			<div class="col-md-7"></div>
 			<div class="col-md-5">
-				<input type="hidden" value="<?php echo $obj->data->results[0]->id ?>" />
+				<input type="hidden" value="<?php echo $obj->data->results[$i]->id ?>" />
 				<a data-toggle="modal" data-target="#deleteMessage" class="btn btn-view">VIEW MORE</a>
 			</div>
 			<div class="col-md-12">				
 			<div class="col-md-12"><span class="subTitleHero">Related commics</span></div>
 				<div class="col-md-6">
-					<?php echo $obj->data->results[0]->comics->items[0]->name ?>
+					<?php echo $obj->data->results[$i]->comics->items[0]->name ?>
 				</div>
 				<div class="col-md-6">
-					<?php echo $obj->data->results[0]->comics->items[1]->name ?>
+					<?php echo $obj->data->results[$i]->comics->items[1]->name ?>
 				</div>
 				<div class="col-md-6">
-					<?php echo $obj->data->results[0]->comics->items[2]->name ?>
+					<?php echo $obj->data->results[$i]->comics->items[2]->name ?>
 				</div>
 				<div class="col-md-6">
-					<?php echo $obj->data->results[0]->comics->items[3]->name ?>
+					<?php echo $obj->data->results[$i]->comics->items[3]->name ?>
 				</div>
 			</div>
 		</div>
 <?php
+	}
 }else{
 	echo '<div id="contenedor">
                  <div class="loader" id="loader">Loading...</div>
--- lib/configure.php
+++ lib/configure.php
@@ -8,5 +8,5 @@ $CGD['cod']['hash'] = md5($CGD['cod']['ts'].$CGD['cod']['private'].$CGD['cod']['
 //URL
 
 $CGD['cod']['url']['all'] = "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=".$CGD['cod']['public']."&hash=".$CGD['cod']['hash'];
-$CGD['cod']['url']['name'] = "https://gateway.marvel.com:443/v1/public/characters?ts=1&name=";
+$CGD['cod']['url']['name'] = "https://gateway.marvel.com:443/v1/public/characters?ts=1&nameStartsWith=";
 ?>
